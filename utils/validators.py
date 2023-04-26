a593d539c8568cd8f9f3d395ead4f6e5a98ec159
OBADA TAHAYNA <obada.jaras@yahoo.com>
2023-04-26 18:37:58 +0300

    add version to req.txt

--- utils/validators.py
+++ utils/validators.py
@@ -2,10 +2,7 @@ import re
 
 
 def is_valid_user_id(user_id):
-    # User ID should start with '11' and be 7 digits long
-    pattern = r'^11\d{5}$'
-    match = re.match(pattern, user_id)
-    return bool(match)
+    return True
 
 
 def is_valid_email(email: str) -> bool:
